5fe5f65b0432e948747d2fa4a04923429c6a37f3
Sidharth Mishra <sidmishraw@gmail.com>
2018-10-14 01:23:33 -0700

    finished implementing the arraybased merkle tree v0.1

--- util/arraybased_merkel.go
+++ util/arraybased_merkel.go
@@ -41,19 +41,34 @@ package util
 // An array based implementation of the Merkel tree.
 // ----
 
-// ArrayBasedMerkle is a merkle tree that uses an array for storing the entire tree,
-// instead of the usual node based tree structure.
+// ArrayBasedAutoEvenerMerkle is a merkle tree that uses an array for storing the entire tree,
+// instead of the usual node based tree structure. It also automatically concatenates odd'th node with itself,
+// when computing the hashes for next level.
 //
-type ArrayBasedMerkle struct {
-	leaves []string
+type ArrayBasedAutoEvenerMerkle struct {
+	nodes []string
+	// the number of leaf nodes is fixed at the time of construction of this merkle
+	// tree, and it is always even
+	nbrLeafNodes uint
 }
 
 // NewArrayBasedMerkle creates a new array based merkle tree instance with 0 nodes
-func NewArrayBasedMerkle() *ArrayBasedMerkle {
+func NewArrayBasedMerkle() *ArrayBasedAutoEvenerMerkle {
 
-	return &ArrayBasedMerkle{
-		leaves: make([]string, 0),
+	return &ArrayBasedAutoEvenerMerkle{
+		nodes: make([]string, 0),
 	}
+}
+
+// GetNumberOfLeafNodes gets the number of leaf nodes of the merkle tree.
+//
+func (merkle *ArrayBasedAutoEvenerMerkle) GetNumberOfLeafNodes() uint {
+
+	if merkle.nbrLeafNodes%2 != 0 {
+		return merkle.nbrLeafNodes + 1
+	}
+
+	return merkle.nbrLeafNodes
 }
 
 // recursiveConstruct takes a list of leaf nodes and recursive hash + merges them till it
@@ -90,26 +105,27 @@ func recursiveConstruct(leaves []string, roundLeafStartIndex int) []string {
 // Assumption 1: Since, the name is `hashes`, I'm assuming that these
 // entities are already hashed (leaves).
 //
-func (merkle *ArrayBasedMerkle) ComputeTree(hashes []Hashable) {
+func (merkle *ArrayBasedAutoEvenerMerkle) ComputeTree(hashes []Hashable) {
 	// Add the leaf nodes to the merkle tree
 	//
-	merkle.leaves = make([]string, len(hashes))
-	for index := 0; index < len(merkle.leaves); index++ {
-		merkle.leaves[index] = hashes[index].GetHash()
+	merkle.nodes = make([]string, len(hashes))
+	merkle.nbrLeafNodes = uint(len(hashes))
+	for index := 0; index < len(merkle.nodes); index++ {
+		merkle.nodes[index] = hashes[index].GetHash()
 	}
 
 	// compute the tree recursively, the last node is the root
 	//
-	merkle.leaves = recursiveConstruct(merkle.leaves, 0)
+	merkle.nodes = recursiveConstruct(merkle.nodes, 0)
 }
 
 // GetRoot gets the hashvalue of the root of the merkle tree. It is the last element of the array.
 //
-func (merkle *ArrayBasedMerkle) GetRoot() string {
+func (merkle *ArrayBasedAutoEvenerMerkle) GetRoot() string {
 
-	if len(merkle.leaves) > 0 {
+	if len(merkle.nodes) > 0 {
 
-		return merkle.leaves[len(merkle.leaves)-1]
+		return merkle.nodes[len(merkle.nodes)-1]
 	}
 
 	return ""
@@ -117,20 +133,25 @@ func (merkle *ArrayBasedMerkle) GetRoot() string {
 
 // GetTree gets the total merkle tree.
 //
-func (merkle *ArrayBasedMerkle) GetTree() []string {
+func (merkle *ArrayBasedAutoEvenerMerkle) GetTree() []string {
 
-	return merkle.leaves
+	return merkle.nodes
 }
 
-// GetPath provides the path for the given hashable entity.
+// GetPath provides the merkle path for the given hashable entity. Returns `nil` if the
+// hash doesn't exist in the merkle tree.
 //
-func (merkle *ArrayBasedMerkle) GetPath(hash Hashable) *MTPath {
+func (merkle *ArrayBasedAutoEvenerMerkle) GetPath(hash Hashable) *MTPath {
 	hashHash := hash.GetHash()
 
 	leafIndex := -1
 
-	for index, hashValue := range merkle.leaves {
-		if hashValue == hashHash {
+	nbrLeafNodes := Evenify(merkle.nbrLeafNodes)
+
+	// find the leaf index in the merkle tree
+	//
+	for index := 0; index < int(nbrLeafNodes); index++ {
+		if merkle.nodes[index] == hashHash {
 			leafIndex = index
 			break
 		}
@@ -154,19 +175,108 @@ func (merkle *ArrayBasedMerkle) GetPath(hash Hashable) *MTPath {
 		LeafIndex: leafIndex,
 	}
 
-	if leafIndex%2 == 0 {
-		handleEvenScenario(merkle.leaves, merklePath)
-	} else {
-		handleOddScenario(merkle.leaves, merklePath)
-	}
+	// Compute the merkle path for the leafIndex.
+	//
+	merkle.computeMerklePath(uint(leafIndex), &merklePath.Nodes, 0, 0)
 
 	return merklePath
 }
 
-func handleEvenScenario(treeNodes []string, merklePath *MTPath) {
+// computeMerklePath computes the merkle path for the given leaf-node. The `pathNodes` is the reference
+// to the array of nodes that make up the path -- all the way upto the root from the leaf node -- the `prevLevelLeafCount`
+// is the number of leaf nodes from the previous level, and `totalLeavesConsumed` refers to the total number
+// of nodes preceeding the leafNode in the current level or iteration.
+//
+// Disclaimer: this is a recursive approach!
+//
+func (merkle *ArrayBasedAutoEvenerMerkle) computeMerklePath(leafIndex uint, pathNodes *[]string, prevLevelLeafCount uint, totalLeavesConsumed uint) {
 
+	*pathNodes = append(*pathNodes, merkle.nodes[leafIndex])
+
+	if leafIndex == uint(len(merkle.nodes)-1) {
+		// reached the root and the path is complete
+		//
+		return
+	}
+
+	// eg - 6 for index 7
+	// eg - 0 for index 1
+	//
+	leafIndex = Leftify(leafIndex)
+
+	// eg - (8 - 6) / 2 = 1 (0-indexed)
+	// eg - (10 - 6 - 4) / 2 = 0 (0 - indexed)
+	//
+	bucketNbr := (leafIndex - totalLeavesConsumed) / 2
+
+	var thisLevelLeafCount uint
+	if prevLevelLeafCount == 0 {
+		thisLevelLeafCount = Evenify(merkle.nbrLeafNodes) // eg - 6
+	} else {
+		thisLevelLeafCount = Evenify(prevLevelLeafCount / 2) // eg - 6/2 + 1 = 4
+	}
+
+	// eg - 6 + 4 = 10
+	//
+	nextLevelLeafIndex := leafIndex + (thisLevelLeafCount - bucketNbr)
+
+	merkle.computeMerklePath(nextLevelLeafIndex, pathNodes, thisLevelLeafCount, prevLevelLeafCount+thisLevelLeafCount)
 }
 
-func handleOddScenario(treeNodes []string, merklePath *MTPath) {
+// Evenify evens out a number, if it is odd, it returns the next even number
+// else just returns the same number as is.
+//
+func Evenify(n uint) uint {
+	if n%2 == 0 {
+		return n
+	}
+	return n + 1
+}
 
+// Leftify computes the left node index of the pair of the given node.
+//
+func Leftify(n uint) uint {
+	if n%2 == 0 {
+		return n
+	}
+	return n - 1
+}
+
+// VerifyPath verifies a given leaf node and the path by computing the merkle-path for the leaf node and
+// diffing it against the given path.
+//
+func (merkle *ArrayBasedAutoEvenerMerkle) VerifyPath(hash Hashable, path *MTPath) bool {
+	calculatedPath := merkle.GetPath(hash)
+	if calculatedPath == nil {
+		return false
+	}
+	if calculatedPath.LeafIndex != path.LeafIndex {
+		return false
+	}
+	for i, v := range calculatedPath.Nodes {
+		if path.Nodes[i] != v {
+			return false
+		}
+	}
+	return true
+}
+
+// GetPathByIndex gets the merkle path for the leaf node given the index.
+// Disclaimer: the data verification of the leaf node is out of the scope for this implementation.
+//
+func (merkle *ArrayBasedAutoEvenerMerkle) GetPathByIndex(idx int) *MTPath {
+	if idx >= int(Evenify(merkle.nbrLeafNodes)) || idx < 0 {
+		return nil
+	}
+	merklePath := &MTPath{
+		Nodes:     make([]string, 0),
+		LeafIndex: idx,
+	}
+	// since the leaf index is know here, we simply go ahead and compute the
+	// merkle path for it.
+	//
+	// Data verification is out of the scope --
+	//
+	merkle.computeMerklePath(uint(idx), &merklePath.Nodes, 0, 0)
+	return merklePath
 }
--- util/merkel_test.go
+++ util/merkel_test.go
@@ -72,3 +72,211 @@ func TestRecursiveConstructEvenTree(t *testing.T) {
 		t.Error("Expected merkle tree to have 13 nodes")
 	}
 }
+
+func TestComputeMerklePathFor_b(t *testing.T) {
+
+	hashes := [6]Hashable{H{"a"}, H{"b"}, H{"c"}, H{"d"}, H{"e"}, H{"f"}}
+	merkleTree := NewArrayBasedMerkle()
+
+	merkleTree.ComputeTree(hashes[:])
+
+	fmt.Printf("merkle root = %s \n", merkleTree.GetRoot())
+	fmt.Printf("merkle leaf count = %d \n", merkleTree.GetNumberOfLeafNodes())
+	fmt.Printf("merkle tree  = %v \n", merkleTree.GetTree())
+	fmt.Printf("merkle path = %v  \n", merkleTree.GetPath(H{"b"}))
+
+	for i, v := range merkleTree.GetPath(H{"b"}).Nodes {
+		switch i {
+		case 0:
+			// b
+			//
+			if v != Hash("b") {
+				t.Errorf("index 0 - Expected %s but found %s \n", Hash("b"), v)
+			}
+		case 1:
+			// ab
+			//
+			if v != Hash(Hash("a")+Hash("b")) {
+				t.Errorf("index 1 -Expected %s but found %s \n", Hash(Hash("a")+Hash("b")), v)
+			}
+		case 2:
+			// abcd
+			//
+			if v != Hash(Hash(Hash("a")+Hash("b"))+Hash(Hash("c")+Hash("d"))) {
+				t.Errorf("index 2 -Expected %s but found %s \n", Hash(Hash(Hash("a")+Hash("b"))+Hash(Hash("c")+Hash("d"))), v)
+			}
+		case 3:
+			// abcdefef
+			//
+			if v != Hash(Hash(Hash(Hash("a")+Hash("b"))+Hash(Hash("c")+Hash("d")))+Hash(Hash(Hash("e")+Hash("f"))+Hash(Hash("e")+Hash("f")))) {
+				t.Errorf("index 3 -Expected %s but found %s \n", Hash(Hash(Hash(Hash("a")+Hash("b"))+Hash(Hash("c")+Hash("d")))+Hash(Hash(Hash("e")+Hash("f"))+Hash(Hash("e")+Hash("f")))), v)
+			}
+		}
+	}
+}
+
+func TestComputeMerklePathFor_c(t *testing.T) {
+
+	hashes := [7]Hashable{H{"a"}, H{"b"}, H{"c"}, H{"d"}, H{"e"}, H{"f"}, H{"g"}}
+	merkleTree := NewArrayBasedMerkle()
+
+	merkleTree.ComputeTree(hashes[:])
+
+	fmt.Printf("merkle root = %s \n", merkleTree.GetRoot())
+	fmt.Printf("merkle leaf count = %d \n", merkleTree.GetNumberOfLeafNodes())
+	fmt.Printf("merkle tree  = %v \n", merkleTree.GetTree())
+	fmt.Printf("merkle path = %v  \n", merkleTree.GetPath(H{"c"}))
+
+	for i, v := range merkleTree.GetPath(H{"c"}).Nodes {
+		switch i {
+		case 0:
+			// c
+			//
+			if v != Hash("c") {
+				t.Errorf("index 0 - Expected %s but found %s \n", Hash("c"), v)
+			}
+		case 1:
+			// cd
+			//
+			if v != Hash(Hash("c")+Hash("d")) {
+				t.Errorf("index 1 -Expected %s but found %s \n", Hash(Hash("c")+Hash("d")), v)
+			}
+		case 2:
+			// abcd
+			//
+			if v != Hash(Hash(Hash("a")+Hash("b"))+Hash(Hash("c")+Hash("d"))) {
+				t.Errorf("index 2 -Expected %s but found %s \n", Hash(Hash(Hash("a")+Hash("b"))+Hash(Hash("c")+Hash("d"))), v)
+			}
+		case 3:
+			// abcdefgg
+			//
+			if v != Hash(Hash(Hash(Hash("a")+Hash("b"))+Hash(Hash("c")+Hash("d")))+Hash(Hash(Hash("e")+Hash("f"))+Hash(Hash("g")+Hash("g")))) {
+				t.Errorf("index 3 -Expected %s but found %s \n", Hash(Hash(Hash(Hash("a")+Hash("b"))+Hash(Hash("c")+Hash("d")))+Hash(Hash(Hash("e")+Hash("f"))+Hash(Hash("g")+Hash("g")))), v)
+			}
+		}
+	}
+
+	t.Logf("Found merkle path to be %v", *merkleTree.GetPath(H{"c"}))
+}
+
+func TestComputeMerklePathFor_2leaves_b(t *testing.T) {
+
+	hashes := [2]Hashable{H{"a"}, H{"b"}}
+	merkleTree := NewArrayBasedMerkle()
+
+	merkleTree.ComputeTree(hashes[:])
+
+	fmt.Printf("merkle root = %s \n", merkleTree.GetRoot())
+	fmt.Printf("merkle leaf count = %d \n", merkleTree.GetNumberOfLeafNodes())
+	fmt.Printf("merkle tree  = %v \n", merkleTree.GetTree())
+	fmt.Printf("merkle path = %v  \n", merkleTree.GetPath(H{"b"}))
+
+	for i, v := range merkleTree.GetPath(H{"b"}).Nodes {
+		switch i {
+		case 0:
+			// b
+			//
+			if v != Hash("b") {
+				t.Errorf("index 0 - Expected %s but found %s \n", Hash("c"), v)
+			}
+		case 1:
+			// ab
+			//
+			if v != Hash(Hash("a")+Hash("b")) {
+				t.Errorf("index 1 -Expected %s but found %s \n", Hash(Hash("a")+Hash("b")), v)
+			}
+		}
+	}
+
+	t.Logf("Found merkle path to be %v", *merkleTree.GetPath(H{"b"}))
+}
+
+func TestVerifyPath(t *testing.T) {
+
+	hashes := [2]Hashable{H{"a"}, H{"b"}}
+	merkleTree := NewArrayBasedMerkle()
+
+	merkleTree.ComputeTree(hashes[:])
+
+	fmt.Printf("merkle root = %s \n", merkleTree.GetRoot())
+	fmt.Printf("merkle leaf count = %d \n", merkleTree.GetNumberOfLeafNodes())
+	fmt.Printf("merkle tree  = %v \n", merkleTree.GetTree())
+	fmt.Printf("merkle path = %v  \n", merkleTree.GetPath(H{"b"}))
+
+	nodes1 := [2]string{Hash(H{"b"}.v), Hash(Hash(H{"a"}.v) + Hash(H{"b"}.v))}
+
+	nodes2 := [2]string{Hash(H{"c"}.v), Hash(Hash(H{"a"}.v) + Hash(H{"b"}.v))}
+
+	myMerkelPath := &MTPath{
+		LeafIndex: 1,
+		Nodes:     nodes1[:],
+	}
+	status := merkleTree.VerifyPath(H{"b"}, myMerkelPath)
+	if !status {
+		t.Error("Was expecting true got false")
+	}
+
+	myMerkelPath2 := &MTPath{
+		LeafIndex: -1,
+		Nodes:     nodes2[:],
+	}
+	status2 := merkleTree.VerifyPath(H{"c"}, myMerkelPath2)
+	if status2 {
+		t.Error("Was expecting false got true")
+	}
+}
+
+func TestGetPathByIndex_c(t *testing.T) {
+
+	hashes := [7]Hashable{H{"a"}, H{"b"}, H{"c"}, H{"d"}, H{"e"}, H{"f"}, H{"g"}}
+	merkleTree := NewArrayBasedMerkle()
+
+	merkleTree.ComputeTree(hashes[:])
+
+	fmt.Printf("merkle root = %s \n", merkleTree.GetRoot())
+	fmt.Printf("merkle leaf count = %d \n", merkleTree.GetNumberOfLeafNodes())
+	fmt.Printf("merkle tree  = %v \n", merkleTree.GetTree())
+	fmt.Printf("merkle path = %v  \n", merkleTree.GetPath(H{"c"}))
+
+	nodes := [4]string{Hash("c"), Hash(Hash("c") + Hash("d")), Hash(Hash(Hash("a")+Hash("b")) + Hash(Hash("c")+Hash("d"))), Hash(Hash(Hash(Hash("a")+Hash("b"))+Hash(Hash("c")+Hash("d"))) + Hash(Hash(Hash("e")+Hash("f"))+Hash(Hash("g")+Hash("g"))))}
+
+	myMerklePath := &MTPath{
+		LeafIndex: 2,
+		Nodes:     nodes[:],
+	}
+
+	t.Logf("my merkle path = %v", *myMerklePath)
+
+	calculatedMPath := merkleTree.GetPathByIndex(2)
+
+	t.Logf("calculated path = %v", *calculatedMPath)
+
+	if myMerklePath.LeafIndex != calculatedMPath.LeafIndex {
+		t.Error("The leaf indices don't match")
+	}
+
+	for i, v := range myMerklePath.Nodes {
+		if calculatedMPath.Nodes[i] != v {
+			t.Errorf("The nodes at index %d don't match in values: got %s but expected %s", i, calculatedMPath.Nodes[i], v)
+		}
+	}
+
+}
+
+// H is a stub used for testing the merkle tree
+//
+type H struct {
+	v string
+}
+
+// GetHash makes H conform to Hashable
+//
+func (h H) GetHash() string {
+	return Hash(h.v)
+}
+
+// GetHashBytes makes H conform to Hashable
+//
+func (h H) GetHashBytes() []byte {
+	return []byte(h.GetHash())
+}
